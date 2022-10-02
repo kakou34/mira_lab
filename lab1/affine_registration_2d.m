c9b193f0e6f198c62df37350f8cf7654a2b70e0e
jseia <joacoseia18@gmail.com>
2022-10-02 10:25:13 +0200

    fix M

--- lab1/affine_registration_2d.m
+++ lab1/affine_registration_2d.m
@@ -1,47 +1,57 @@
-function [Icor, M] = affine_registration_2d(Im, If, mtype, ttype)
-%Example of 2D affine registration
-%   Robert Martí  (robert.marti@udg.edu)
-%   Based on the files from  D.Kroon University of Twente 
-% mtype = 'cc'; % metric type: sd: ssd gcc: gradient correlation; cc: cross-correlation
-% ttype = 'r'; % rigid registration, options: r: rigid, a: affine
+function [img_reg, M] = affine_registration_2d(img_mov, img_fix, mtype, ttype)
+% This function estimates the parameters of a 2D affine ranformation
+%
+% img_reg, M = affine_registration_2d(img_mov, img_fix, mtype, ttype)
+%
+% inputs,
+%   img_mov: The moving image
+%   img_fix: The fixed image
+%   mtype: metric type:
+%       'sd': ssd 
+%       'nncc': negative normalized cross-correlation
+%       'nngcc': negative normalized gradient correlation
+%   ttype: registration type, options:
+%       'r': rigid
+%       'a': affine
+% output,
+%   img_out: The transformed image
 
-% Parameter scaling of the Translation and Rotation
-% and initial parameters
+% Parameter scaling of the Translation and Rotation and initial parameters
 switch ttype
     case 'r'
-        x=[0 0 0];
+        x = [0 0 0];
         scale = [1 1 0.1];
     case 'a'
-        x=[0 0 0 0 0 0 0];
+        x = [0 0 0 1 1 0 0];
         scale = [1 1 0.1 1 1 1 1];
 end
 
-x=x./scale;
+x = x./scale;
 
-[x]=fminsearch(@(x)affine_registration_function(x,scale,Im,If,mtype,ttype),x,optimset('Display','iter','MaxIter',1000, 'TolFun', 1.000000e-10,'TolX',1.000000e-10, 'MaxFunEvals', 1000*length(x)));
+[x] = fminsearch(...
+    @(x)affine_registration_function(...
+        x, scale, img_mov, img_fix, mtype, ttype), ...
+    x, optimset('Display', 'iter', 'MaxIter', 1000, ...
+        'TolFun', 1.000000e-10, 'TolX', 1.000000e-10, ...
+        'MaxFunEvals', 1000*length(x)));
 
-x=x.*scale;
-disp(x)
+x = x.*scale;
+
+% Obtain the affine transformation matrix
 switch ttype
-	case 'r'
-        % Make the affine transformation matrix
-        M = [ cos(x(3)) sin(x(3)) x(1);
-            -sin(x(3)) cos(x(3)) x(2);
+    case 'r'
+        M = [cos(x(3)) sin(x(3)) x(1);
+             -sin(x(3)) cos(x(3)) x(2);
              0 0 1];
+%         M = inv(M);
     case 'a'
-        M = [ cos(x(3))*x(4) sin(x(3)) x(1);
+        M = [cos(x(3))*x(4) sin(x(3)) x(1);
              -sin(x(3)) cos(x(3))*x(5) x(2);
-              x(6) x(7) 1];
+             x(6) x(7) 1];
+%         M = inv(M);
 end
 
- % Transform the image 
-Icor=affine_transform_2d_double(double(Im),double(M),0); % 3 stands for cubic interpolation
-
-% Show the registration results
-% figure,
-% subplot(2,2,1), imshow(If);
-% subplot(2,2,2), imshow(Im);
-% subplot(2,2,3), imshow(Icor);
-% subplot(2,2,4), imshow(abs(If-Icor));
+% Transform the image 
+img_reg = affine_transform_2d_double(double(img_mov), double(M), 1);
 end
 
